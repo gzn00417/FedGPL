72b87d4abbff6ab0889f7ce06a984ab09f80d44c
Ruiqian HAN <1138146340@qq.com>
2024-05-23 00:23:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,10 +20,6 @@ if __name__ == '__main__':
     args = args_parser()
     pl.seed_everything(args.seed)
     print(args.few_shot)
-    if args.algorithm == 'VPG':
-        wandb.run.name = str(args.dataset_name) + '_' + str(args.federated) + '_' + 'E+G' + '_' + str(args.algorithm) + '' + str(args.ratio) + str(args.few_shot) + 'new'
-    else:
-        wandb.run.name = str(args.dataset_name) + '_' + str(args.federated) + '_' + 'E+G' + '_' + str(args.algorithm) + '' + str(args.cross_prune) + str(args.few_shot) + 'new'
     # model
     pre_trained_gnn = GNN(args.input_dim, hid_dim=args.hidden_dim, out_dim=args.hidden_dim, gcn_layer_num=2, gnn_type=args.gnn_type)
     pre_trained_gnn.load_state_dict(torch.load(f'./pre_trained_gnn/{args.dataset_name}.{args.pre_train_algorithm}.{args.gnn_type}.pth', map_location='cuda:3'))
